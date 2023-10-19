54b2dd9f3cba43e256eb58a88d35723db30ca31b
Vitor Domingos <vm486092@gmail.com>
2023-10-19 15:14:30 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+// Loop for que vai de 0 a 99 (100 iterações)
+for (let i = 0; i < 100; i++) {
+    // A cada iteração, imprime a mensagem "Eu amo programar em javascript"
+    console.log("Eu amo programar em javascript");
+}
+
+// Loop for que vai de 0 a 199 (200 iterações)
+for (let i = 0; i < 200; i++) {
+    // A cada iteração, imprime a mensagem "JavaScript é muito legal"
+    console.log("JavaScript é muito legal");
+}
